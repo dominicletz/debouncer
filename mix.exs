cb2a9e3853b151c08afcf88365b83519a844848e
Dominic Letz <dominic@diode.io>
2022-05-18 12:24:53 +0200

    v0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Debouncer.MixProject do
   def project do
     [
       app: :debouncer,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
