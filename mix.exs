c56c944271c8b44f447a1d37bc0d6607eea51bb9
Dominic Letz <dominic.letz@berlin.de>
2023-10-17 15:46:36 +0200

    v0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Debouncer.MixProject do
   def project do
     [
       app: :debouncer,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
